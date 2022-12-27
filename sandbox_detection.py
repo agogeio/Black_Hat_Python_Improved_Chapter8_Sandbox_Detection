163fad87c8cb8be6ae6fc4b90caf4905af2755c3
agogeio <steven.aiello@agoge.io>
2022-12-27 15:51:43 -0500

    Sandbox updates

--- sandbox_detection.py
+++ sandbox_detection.py
@@ -13,13 +13,22 @@ from pynput import keyboard
 #? We will focus on evading detecion on Windows systems 
 
 SLEEP_TIMER = 520
-DAY_THRESHOLD = 1
-HOUR_THRESHOLD = 8
-MIN_THRESHOLD = 0
 
-MEASURE_TIME = 5 * 60
+SRV_MONTH_THRESHOLD = 1
+SRV_DAY_THRESHOLD = 5
+SRV_HOUR_THRESHOLD = 0
+SRV_MIN_THRESHOLD = 0
+
+WORKSTATION_MONTH_THRESHOLD = 0
+WORKSTATION_DAY_THRESHOLD = 0
+WORKSTATION_HOUR_THRESHOLD = 8
+WORKSTATION_MIN_THRESHOLD = 0
+
 WORKSTATION_KEY_COUNT = 50
 SERVER_KEY_COUNT = 10
+
+MEASURE_TIME = 5 * 60
+
 
 
 #? Objectives:
@@ -41,12 +50,12 @@ class SBD:
 
         if os_platform == 'Windows':
             result = subprocess.run('systeminfo', capture_output=True, text=True, universal_newlines=True)
-
-            if 'Server' in result.stdout:
-                print('The OS is likely Windows Server')
+            # print(f'Result: {result.stdout}')
+            if 'Microsoft Windows Server' in result.stdout:
+                # print('The OS is likely Windows Server')
                 return 'Windows Server'
             else:
-                print('The OS is likely a Windows workstation')
+                # print('The OS is likely a Windows workstation')
                 return 'Windows Workstation'
 
 
@@ -61,7 +70,7 @@ class SBD:
         for line in lines:
             if search_term in line:
                 system_boot = line.split('          ')[1]
-                print(system_boot)
+                # print(system_boot)
 
                 system_boot_date = system_boot.split(',')[0]
                 system_boot_hr_min_sec = system_boot.split(',')[1]
@@ -133,15 +142,39 @@ if __name__ == '__main__':
     sbd = SBD()
     # sbd.hibernate(SLEEP_TIMER)
     os = sbd.get_os()
-    print(os)
+    # print(os)
     diff = sbd.get_boot_diff()
     sbd.count_keys()
 
     while True:
 
-        if WORKSTATION_KEY_COUNT < sbd.get_key_count():
-            print('Workstation key count hit')
+        if os == "Windows Server":
+
+            if diff['day'] >= SRV_DAY_THRESHOLD or \
+                diff['month'] >= SRV_MONTH_THRESHOLD or \
+                diff['hour'] >= SRV_HOUR_THRESHOLD:
+                print('Server uptime pass')
+
+                # print("Windows Server")
+                if WORKSTATION_KEY_COUNT < sbd.get_key_count():
+                    print('Workstation key count hit')
+                else:
+                    # Key count not hit
+                    pass
+
+        elif os == "Windows Workstation":
+
+            if diff['day'] >= WORKSTATION_DAY_THRESHOLD or diff['hour'] >= WORKSTATION_HOUR_THRESHOLD:
+                print('Workstation uptime pass')
+
+                # print("Windows Workstation")
+                if WORKSTATION_KEY_COUNT < sbd.get_key_count():
+                    print('Workstation key count hit')
+                else:
+                    # Key count not hit
+                    pass
+        
         else:
-            print('Keep typing')
+            print('Likely Linux or MacOS')
 
         time.sleep(SLEEP_TIMER)
